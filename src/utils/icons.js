ef1e4b892a2e006a5c9fe58fb495f5b18011881f
shihab-shamim <shihabshamim767@gmail.com.com>
2026-06-17 14:42:45 +0600

    complete code

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,15 +1,15 @@
 const iconColor = '#4527a4';
 
 export const blockIcon = <svg xmlns='http://www.w3.org/2000/svg' width={24} height={24} viewBox='0 0 24 24'>
-	{/* Card background */}
+	
 	<rect fill={iconColor} opacity='0.12' x='2' y='1' width='20' height='22' rx='3' />
-	{/* Card border */}
+	
 	<rect fill='none' stroke={iconColor} strokeWidth='1.5' x='2' y='1' width='20' height='22' rx='3' />
-	{/* Avatar circle */}
+	
 	<circle fill={iconColor} cx='12' cy='9' r='4' />
-	{/* Name bar */}
+	
 	<rect fill={iconColor} x='5' y='16' width='14' height='2' rx='1' />
-	{/* Designation bar */}
+	
 	<rect fill={iconColor} x='7.5' y='19.5' width='9' height='1.5' rx='0.75' opacity='0.5' />
 </svg>;
 
